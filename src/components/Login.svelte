8d1f3e98b68614b74fd72b3c022620a333ad09ed
silvestrevivo <silvestrevivomillan@gmail.com>
2020-04-23 23:52:29 +0200

    authentication finished

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,22 +1,24 @@
 <script>
-    import { goto, stores } from '@sapper/app';
+  import { goto, stores } from "@sapper/app";
+  import { post } from "auth.js";
 
-    const { session } = stores();
+  const { session } = stores();
 
-    let name = '';
-    let email = '';
-    let password = '';
-    let role = '';
+  let email = "";
+  let password = "";
 
-    // Function to call api to login a user
-    async function submit(event) {
-        // Call login route and authenticate
+  // Function to call api to login a user
+  async function submit(event) {
+    const response = await post(`auth/login`, { email, password });
 
-        // add auth user to session
+    // TODO handle network errors
+    // errors = response.errors;
 
-        // Route logged in user to Dashboard
-        goto('/dashboard');
+    if (response.user) {
+      $session.user = response.user;
+      goto("/dashboard");
     }
+  }
 </script>
 
 <style>
@@ -24,17 +26,28 @@
 </style>
 
 <svelte:head>
-	<title>Login</title>
+  <title>Login</title>
 </svelte:head>
 
 <form on:submit|preventDefault={submit}>
-    <fieldset class="form-group">
-        <input class="form-control form-control-lg" type="email" placeholder="Email" bind:value={email}>
-    </fieldset>
-    <fieldset class="form-group">
-        <input class="form-control form-control-lg" type="password" placeholder="Password" bind:value={password}>
-    </fieldset>
-    <button class="btn btn-lg btn-primary pull-xs-right" type="submit" disabled='{!email || !password}'>
-        Login
-    </button>
+  <fieldset class="form-group">
+    <input
+      class="form-control form-control-lg"
+      type="email"
+      placeholder="Email"
+      bind:value={email} />
+  </fieldset>
+  <fieldset class="form-group">
+    <input
+      class="form-control form-control-lg"
+      type="password"
+      placeholder="Password"
+      bind:value={password} />
+  </fieldset>
+  <button
+    class="btn btn-lg btn-primary pull-xs-right"
+    type="submit"
+    disabled={!email || !password}>
+    Login
+  </button>
 </form>
