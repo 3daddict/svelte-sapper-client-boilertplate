16d8b61ccf90ad40e72249abd2e35dfdce0b1fbc
silvestrevivo <silvestrevivomillan@gmail.com>
2020-05-11 22:51:06 +0200

    token available in client

--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -4,7 +4,7 @@ export function post(req, res) {
 	const user = req.body;
 
 	api.post('auth/login', user ).then(response => {
-		if (response.user) req.session.user = response.user;
+		if (response.user) req.session.user = response.user; req.session.token = response.token;
 		res.end(JSON.stringify(response));
 	});
 }
