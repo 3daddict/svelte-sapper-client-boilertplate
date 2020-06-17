b357d204238d79b3c43a091543d9b450f5370dc0
silvestrevivo <silvestrevivomillan@gmail.com>
2020-06-17 09:24:21 +0200

    $: projects = accountData.projects; on line 26 to make projects reactive

--- src/routes/account/[_id].svelte
+++ src/routes/account/[_id].svelte
@@ -1,49 +1,56 @@
 <script context="module">
-	import * as api from "api.js";
+  import * as api from "api.js";
 
-	export async function preload(page, session)  {
-		//console.log('page', page);
-		//console.log('session', session);
-		const { _id } = page.params;
+  export async function preload(page, session) {
+    //console.log('page', page);
+    //console.log('session', session);
+    const { _id } = page.params;
 
-		const res = await api.get(`accounts/${_id}`, session.token, session.user._id)
-		const accountData = res.data;
-		
-		return { accountData }
-	}
+    const res = await api.get(
+      `accounts/${_id}`,
+      session.token,
+      session.user._id
+    );
+    const accountData = res.data;
+
+    console.log("server _id", accountData);
+
+    return { accountData };
+  }
 </script>
 
 <script>
-	export let accountData;
-	//console.log(accountData.projects)
+  export let accountData;
+  console.log("client _id", accountData);
 
-	const projects = accountData.projects;
-	JSON.stringify(projects)
+  $: projects = accountData.projects;
 
-	console.log(accountData)
-
-	if(accountData) {
-		//activeName = accountData.accountName;
-	}
+  if (accountData) {
+    //activeName = accountData.accountName;
+  }
 </script>
 
 <svelte:head>
-	<title>{accountData.accountName}</title>
+  <title>{accountData.accountName}</title>
 </svelte:head>
-
 
 <h1>{accountData.accountName}</h1>
 
 <ul>
-{#if projects}
-	 {#each projects as project}
-	<li>
-		<a class="hover:underline text-blue-500" href="/project/{project._id}" id="{project._id}">{project.projectName}</a>
-	</li>
-{:else}
-	<li>
-		<p>No projects found or call has not been made</p>
-	</li>
-{/each}
-{/if}
+  {#if projects}
+    {#each projects as project}
+      <li>
+        <a
+          class="hover:underline text-blue-500"
+          href="/project/{project._id}"
+          id={project._id}>
+          {project.projectName}
+        </a>
+      </li>
+    {:else}
+      <li>
+        <p>No projects found or call has not been made</p>
+      </li>
+    {/each}
+  {/if}
 </ul>
